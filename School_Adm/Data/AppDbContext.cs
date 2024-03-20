eb1ed384db0c4bb8c3c4e49eebae72417ce89cc6
Dionisiohenriq <henriquedionisio.dionisioferre@gmail.com>
2024-03-20 01:48:37 -0300

    Add project files.

---
+++ School_Adm/Data/AppDbContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+using School_Adm.Models;
+
+namespace School_Adm.Data;
+
+public class AppDbContext : DbContext
+{
+    public AppDbContext(DbContextOptions options) : base(options)
+    {
+        
+    }
+    public DbSet<Student> Students { get; set; } = null!;
+
+}
---
+++ School_Adm/Models/Student.cs
@@ -0,0 +1,8 @@
+﻿namespace School_Adm.Models;
+
+public class Student
+{
+    public int Id { get; set; }
+    public string Title { get; set; } = string.Empty;
+    public string Description { get; set; } = string.Empty;
+}
